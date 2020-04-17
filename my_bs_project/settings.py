f3a01ea8cf02d18cd1507af51fde550ca7cd2924
neyvis <neyvis.remon.gonzalez@gmail.com>
2020-04-17 14:49:12 -0400

    Deleting STATIC ROOT from settings.py

--- my_bs_project/settings.py
+++ my_bs_project/settings.py
@@ -127,8 +127,5 @@ USE_TZ = True
 #DataFlair #Django #Static files
 STATIC_URL = '/static/'
 #--------------------------------------------------
-#STATIC_ROOT = os.path.join(BASE_DIR, 'root')
-#-----------------------------------------------------
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-]
+
+
