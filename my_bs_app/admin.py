405f36485cd9eca44df9aac6ae4f0286316dfacb
neyvis <neyvis.remon.gonzalez@gmail.com>
2020-04-18 12:31:37 -0400

    Adding a Post model

--- my_bs_app/admin.py
+++ my_bs_app/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from .models import Entry
+from .models import Post
 
 
-class EntryAdmin(admin.ModelAdmin):
-    pass
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
 
 
-admin.site.register(Entry, EntryAdmin)
+admin.site.register(Post, PostAdmin)
--- my_bs_app/models.py
+++ my_bs_app/models.py
@@ -1,8 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Entry(models.Model):
-    name = models.CharField(max_length=50)
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    #slug = models.SlugField(max_length=200, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    image = models.ImageField()
+    content = models.TextField(help_text="Enter your text here.")
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
 
     def __unicode__(self):
         return self.name
