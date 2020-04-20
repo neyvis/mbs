82f4ee500a376298da2b5f62c16951b1e41bafdd
neyvis <neyvis.remon.gonzalez@gmail.com>
2020-04-19 21:31:02 -0400

    Loggin Logout and Register

--- my_bs_app/urls.py
+++ my_bs_app/urls.py
@@ -2,12 +2,16 @@
 
 """
 from django.urls import path
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
-from .views import PostDetail, PostList
+from .views import PostDetail, PostList, login_request, register_request, logout_request
 
 
 urlpatterns = [
     path('', PostList.as_view(), name='home'),
+    path('login/', login_request, name='login'),
+    path('register/', register_request, name='register'),
+    path('logout', logout_request, name='logout'),
     path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
 ]
--- my_bs_app/views.py
+++ my_bs_app/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, TemplateView
 from django.views.generic.detail import DetailView
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
+from django.http import HttpResponse
+
 from .models import Post
+
+from .forms import NewUserForm
 
 
 class PostList(ListView):
-    #queryset = Post.objects.filter(status=1).order_by('-created_on')
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    #queryset = Post.objects.all()
     context_object_name = 'Post list'
     template_name = 'index.html'
     paginate_by = 100
@@ -21,3 +29,55 @@ class PostDetail(DetailView):
     model = Post
     template_name = 'post.html'
 
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("home")
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, "You are now logged in as {username}")
+                print("You are now logged in as {username}")
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request = request,
+                    template_name = "login.html",
+                    context={"form":form})
+
+
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect("home")
+
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+            return render(request = request,
+                          template_name = "register.html",
+                          context={"form":form})
+
+    form = NewUserForm
+    return render(request = request,
+                  template_name = "register.html",
+                  context={"form":form})
